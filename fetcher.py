6b6a1dadcd7f5b451c2b5351644163e826556f13
Noah Stephen Haskell <nstephenh@gmail.com>
2015-09-02 10:07:00 -0400

    createed fetcer.py

---
+++ fetcher.py
@@ -0,0 +1,9 @@
+import urllib
+
+def fetch(comicdef):
+	# comic name, starturl, nextlinkregex, imgregex
+	print(comicdef[0])
+	print(comicdef[1]
+	starturl = comicdef[1]
+	urllib
+			
--- pywoofy.py
+++ pywoofy.py
@@ -1,10 +1,11 @@
 print("Pywoofy")
 
+import fetcher
 keeprunning = 1
 # comic name, starturl, nextlinkregex, imgregex
 comiclist=[["grrl power", """http://grrlpowercomic.com/archives/48", "<a href="(?<content>[^"]*?)" class="navi navi-next" title="Next">Next</a>""","""<img src="(?<content>[^"]*?)" alt="[^"]*?" title="[^"]*?" class="ishadow40" />"""]]
 
 while keeprunning == 1:
-	for comic in comiclist:
-		print(comic[0])
+	for comicdef in comiclist:
+		fetcher.fetch(comicdef)
 	keeprunning = 0
