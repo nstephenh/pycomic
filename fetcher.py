478a72a81c9fdbc46c3f3f9be1a94276b6790837
Noah Stephen Haskell <nstephenh@gmail.com>
2015-09-03 11:09:12 -0400

    fetcher.fetch now downloads the start page

--- fetcher.py
+++ fetcher.py
@@ -1,9 +1,10 @@
-import urllib
+import urllib2
 
 def fetch(comicdef):
 	# comic name, starturl, nextlinkregex, imgregex
 	print(comicdef[0])
-	print(comicdef[1]
+	print(comicdef[1])
 	starturl = comicdef[1]
-	urllib
+	print(urllib2.urlopen(starturl).read())
+
 			
--- pywoofy.py
+++ pywoofy.py
@@ -3,7 +3,7 @@ print("Pywoofy")
 import fetcher
 keeprunning = 1
 # comic name, starturl, nextlinkregex, imgregex
-comiclist=[["grrl power", """http://grrlpowercomic.com/archives/48", "<a href="(?<content>[^"]*?)" class="navi navi-next" title="Next">Next</a>""","""<img src="(?<content>[^"]*?)" alt="[^"]*?" title="[^"]*?" class="ishadow40" />"""]]
+comiclist=[["grrl power", """http://grrlpowercomic.com/archives/48""", """<a href="(?<content>[^"]*?)" class="navi navi-next" title="Next">Next</a>""","""<img src="(?<content>[^"]*?)" alt="[^"]*?" title="[^"]*?" class="ishadow40" />"""]]
 
 while keeprunning == 1:
 	for comicdef in comiclist:
