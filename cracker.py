58ddb7a9cdfed911f2c409679c9e5bbd60cc42e0
Noah Stephen Haskell <nstephenh@gmail.com>
2015-09-08 10:41:35 -0400

    fixed regex

---
+++ cracker.py
@@ -0,0 +1,9 @@
+#seeks out specific parts of the webpage
+#name based off of cracker, a fictional dog detecting booby traps in vietnam
+import re
+
+def findelement(pagefeed, elementregex):
+	return re.split(elementregex, pagefeed)[1]
+
+
+
