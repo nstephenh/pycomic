6f1a0790cac338a71a3da07017858b150798ea19
Noah Stephen Haskell <nstephenh@gmail.com>
2015-09-09 07:30:42 -0400

    Added a function to allow page finding to loop

--- fetcher.py
+++ fetcher.py
@@ -1,10 +1,17 @@
+#Downloads the html available on the specified page
 import urllib2
+import cracker
+def fetchpage(url):
+	try:
+		return urllib2.urlopen(url).read()
+		#The following is for testing purposes only
+		#return """<a href="content/nextpage/example.test" class="navi navi-next" title="Next">Next</a>"""
 
-def fetch(comicdef):
-	# comic name, starturl, nextlinkregex, imgregex
-	print(comicdef[0])
-	print(comicdef[1])
-	starturl = comicdef[1]
-	print(urllib2.urlopen(starturl).read())
-
+	except:
+		print("No internet Connection")	
+		return "No internet Connection"
+def fetchcomic(url):
+	while url != "No internet Connection":
+		#fetch the image here
+		url = cracker.findelement(fetchpage(url))
 			
--- pywoofy.py
+++ pywoofy.py
@@ -1,11 +1,13 @@
 print("Pywoofy")
 
 import fetcher
+import cracker
 keeprunning = 1
 # comic name, starturl, nextlinkregex, imgregex
-comiclist=[["grrl power", """http://grrlpowercomic.com/archives/48""", """<a href="(?<content>[^"]*?)" class="navi navi-next" title="Next">Next</a>""","""<img src="(?<content>[^"]*?)" alt="[^"]*?" title="[^"]*?" class="ishadow40" />"""]]
+comiclist=[["grrl power", """http://grrlpowercomic.com/archives/48""","""<a href="(.*)" class="navi navi-next" title="Next">Next</a>""","""<img srce"(?<content>[^"]*?)" alt="[^"]*?" title="[^"]*?" class="ishadow40" \\\/>"""]]
 
 while keeprunning == 1:
 	for comicdef in comiclist:
-		fetcher.fetch(comicdef)
+		pagefeed = fetcher.fetchpage(comicdef[1])
+		print(cracker.findelement(pagefeed, comicdef[2]))
 	keeprunning = 0
