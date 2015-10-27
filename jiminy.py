f927b135cd9af1deb2e3753c87417461ec2a5e89
Your Name <nstephenh@gmail.com>
2015-10-27 10:44:57 -0400

    worked on jiminy.py

--- jiminy.py
+++ jiminy.py
@@ -1,3 +1,14 @@
 #zip-a-de do-dah
+#Its a pun
+#jiminiy zips comics and spits out .cbz files with the date.
 
+import datetime
+import ZipFile
+
+def makecbz(comicname , downloaddir):
+	timedatestamp = str(datetime.now())
+	outputcbz = ZipFile.open(comicname + "_" +timedatestamp + ".cbz", 'x')
+	for page in os.listdir(downloaddir + "/" + comicname):
+		outputcbz.write(page)
+	print("Sucessfully made .cbz for " + comicname)
 
